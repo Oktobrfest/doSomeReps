0f9c3dce2a4fc7284e5c15743fb0dc8484898ed2
Oktobrfest <mit3@gmx.com>
2022-10-27 00:13:48 +0000

    db work

--- repz/database.py
+++ repz/database.py
@@ -4,13 +4,28 @@ from sqlalchemy.orm import sessionmaker
 
 from setuptools import setup, find_packages
 
+import pyodbc 
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+# server = 'tcp:myserver.database.windows.net' 
+# database = 'mydb' 
+# username = 'myusername' 
+# password = 'mypassword' 
+# # ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
+# cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+# cursor = cnxn.cursor()
+username = 'admin' 
+password = 'Rocking9!'
+ 
+Server = 'repdb.cakbh4mjxv9u.us-west-2.rds.amazonaws.com,1433'
+Database = 'repdb'
+Driver = 'ODBC Driver 18 for SQL Server'
+
+SQLALCHEMY_DATABASE_URL = f'mssql://{username}:{password}@{Server}/{Database}?driver={Driver}'
 
 
-Server = 'LAPTOP-ASUS-LAP\SQLEXPRESS'
-Database = 'reps'
-Driver = 'ODBC Driver 17 for SQL Server'
-
-SQLALCHEMY_DATABASE_URL = f'mssql://@{Server}/{Database}?driver={Driver}'
+# SQLALCHEMY_DATABASE_URL = f'mssql://@{Server}/{Database}?driver={Driver}'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True, future=True)
