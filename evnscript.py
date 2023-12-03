3ead48dd30a72d8df35c4a47fafc505be69ca681
Oktobrfest <mit3@gmx.com>
2023-12-03 05:23:18 +0000

    move git up a folder

---
+++ evnscript.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append('/home/ubuntu/projects/venv/lib/python3.10/site-packages')
+sys.path.append('/home/ubuntu/projects/venv/lib/python3.10/site-packages/python-dotenv')
+sys.path.append('/home/ubuntu/projects/venv/lib/python3.10/site-packages/dotenv')
