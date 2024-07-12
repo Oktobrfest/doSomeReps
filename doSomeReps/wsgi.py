c9d6afd610770e5fb72032bcdd032a9e6d6ccc80
Oktoberfest <daveemail1@protonmail.com>
2024-07-11 23:49:43 -0400

    wsgi cleanup

--- doSomeReps/wsgi.py
+++ doSomeReps/wsgi.py
@@ -1,21 +1,6 @@
-
-"""Application entry point."""
-# print(sys.executable)
-# print(sys.version)
-
 from repz import init_app 
 
 app = init_app()
-# redundant- delete this? app.debug = True
-
-# import sys
-# import logging
-
-# logging.basicConfig(stream=sys.stderr)
-# sys.path.insert(0,"/var/www/html/repz/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5554, use_reloader=True)
-    
-    
-    # redundant- delete this? ,debug=True, in the above .run
