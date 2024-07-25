5725cd6a051cd5eb73af879d8fddd4baa49e6da8
dave <daveemail1@protonmail.com>
2024-07-24 22:30:29 -0400

    lots of cleanup. reorganized routes for better or worse

--- doSomeReps/repz/catz/catz.py
+++ doSomeReps/repz/catz/catz.py
@@ -1,26 +1,11 @@
 from flask import (
-    Blueprint,
     render_template,
-    session as local_session,
+    send_from_directory
 )
 
-from flask import send_from_directory
-
-
-from ..database import session
-
-from ..bluehelpers import *
+from ..bluehelpers import cat_questions_count
 from flask_login import current_user
-
-catz = Blueprint(
-    'catz', __name__,
-    template_folder='templates',
-)
-
-catz_static = Blueprint(
-    'catz_static', __name__,
-    static_folder='static'
-)
+from repz.routes import catz, catz_static
 
 
 @catz.route('/topiclist', methods=['GET', 'POST'], endpoint='topiclist')
@@ -28,8 +13,7 @@ def topiclist():
     topics_list = cat_questions_count(100)
     topics = dict(topics_list)
    
-    # Render the template and pass the categories to it
-    
+    # Render the template and pass the categories to it    
     return render_template('topiclist.html', 
                            user=current_user,
                            topics=topics,
