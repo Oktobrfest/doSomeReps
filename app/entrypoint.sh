0285bb63c1f247fa84fca848e92982e53c558880
Oktobrfest <74153486+Oktobrfest@users.noreply.github.com>
2026-01-12 14:49:45 -0500

    use gunicorn for dev temporarily

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -37,24 +37,31 @@ if [ "$FLASK_ENV" = "development" ] || [ "$FLASK_DEBUG" = "1" ]; then
     npm run build
 
     echo "Development environment detected, installing debugpy..."
-    pip install debugpy
-    if [ "$IDE" = "vscode" ]; then
-        echo "VSCODE DEBUGGING:::::: Starting the application with: python -m debugpy --wait-for-client --listen 0.0.0.0:${DEBUG_PORT} -m flask run --host=0.0.0.0 --port=${APP_PORT} --debugger ;;; the debugger is ${DEBUG_PORT}  and app port is ${APP_PORT} "
-        # IF PROBLEMS WITH DEBUGGER SURFACE- GET RID OF "--debugger"
-        python -m debugpy --wait-for-client --listen 0.0.0.0:${DEBUG_PORT} -m flask run --host=0.0.0.0 --port=${APP_PORT} --debugger --reload
-        #  reloader will likely give problems too!!
-    fi
-    if [ "$IDE" = "pycharm" ]; then
-        pip install pydevd-pycharm==242.10180.30
-        echo "PYCHARM DEBUGGING:::::::"
-        echo "Starting Flask like this: /n flask run --host=0.0.0.0
-        --port=${APP_PORT} --no-reload"
-        flask run --host=0.0.0.0 --port=${APP_PORT} --no-reload
-    fi
-    if [ "$IDE" = "vsdev" ]; then
-     echo "STARTING DEVELOPMENT USING: ${IDE} ; EXCECUTED: flask run --host=0.0.0.0 --port=${APP_PORT} --debugger --reload"
-     flask run --host=0.0.0.0 --port=${APP_PORT} --debugger --reload
-    fi
+    # pip install debugpy
+    # if [ "$IDE" = "vscode" ]; then
+    #     echo "VSCODE DEBUGGING:::::: Starting the application with: python -m debugpy --wait-for-client --listen 0.0.0.0:${DEBUG_PORT} -m flask run --host=0.0.0.0 --port=${APP_PORT} --debugger ;;; the debugger is ${DEBUG_PORT}  and app port is ${APP_PORT} "
+    #     # IF PROBLEMS WITH DEBUGGER SURFACE- GET RID OF "--debugger"
+    #     python -m debugpy --wait-for-client --listen 0.0.0.0:${DEBUG_PORT} -m flask run --host=0.0.0.0 --port=${APP_PORT} --debugger --reload
+    #     #  reloader will likely give problems too!!
+    # fi
+    # if [ "$IDE" = "pycharm" ]; then
+    #     pip install pydevd-pycharm==242.10180.30
+    #     echo "PYCHARM DEBUGGING:::::::"
+    #     echo "Starting Flask like this: /n flask run --host=0.0.0.0
+    #     --port=${APP_PORT} --no-reload"
+    #     flask run --host=0.0.0.0 --port=${APP_PORT} --no-reload
+    # fi
+    # if [ "$IDE" = "vsdev" ]; then
+    #  echo "STARTING DEVELOPMENT USING: ${IDE} ; EXCECUTED: flask run --host=0.0.0.0 --port=${APP_PORT} --debugger --reload"
+    #  flask run --host=0.0.0.0 --port=${APP_PORT} --debugger --reload
+    # fi
+
+ exec gunicorn --workers 2 --threads 2 --keep-alive 5 \
+    --bind 0.0.0.0:${APP_PORT} --worker-tmp-dir /dev/shm \
+    --access-logfile - --error-logfile - --log-level info "wsgi:app"
+
+
+
 else
 #     # flask run --host=0.0.0.0 --port=${APP_PORT}
 
