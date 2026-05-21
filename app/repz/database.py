1fa4d3c0ebf94be6b8a4fa429b42e79a5e82be9f
Oktobrfest <Oktobrfest@users.noreply.github.com>
2026-05-21 15:40:14 -0400

    add __init__ files, pause buttons, unit tests, cleanup minor issues and remove junk fallback audio

--- app/repz/database.py
+++ app/repz/database.py
@@ -4,22 +4,41 @@ from urllib.parse import quote
 
 from flask import current_app
 
-DB_HOST = current_app.config['DB_HOST']
-DB_PORT = current_app.config['DB_PORT']
-SERVER = f'{DB_HOST}:{DB_PORT}'
+def get_db_url():
+    DB_HOST = current_app.config['DB_HOST']
+    DB_PORT = current_app.config['DB_PORT']
+    SERVER = f'{DB_HOST}:{DB_PORT}'
 
-DATABASE = current_app.config['DB_NAME']
-USERNAME = current_app.config['DB_USERNAME']
-PASSWORD = quote(current_app.config['DB_PASSWORD'])
-DRIVER = current_app.config.get('DRIVER', 'psycopg2')
+    DATABASE = current_app.config['DB_NAME']
+    USERNAME = current_app.config['DB_USERNAME']
+    PASSWORD = quote(current_app.config['DB_PASSWORD'])
+    DRIVER = current_app.config.get('DRIVER', 'psycopg2')
 
-SQLALCHEMY_DATABASE_URL = f'postgresql+{DRIVER}://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}'
+    return f'postgresql+{DRIVER}://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}'
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, echo=False, future=True, pool_recycle=1800, pool_pre_ping=True)
+class SessionProxy:
+    def __init__(self):
+        self._session = None
 
-session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine,
-                                         future=True))
+    @property
+    def session(self):
+        if self._session is None:
+            engine = create_engine(
+                get_db_url(), echo=False, future=True, pool_recycle=1800, pool_pre_ping=True)
+
+            self._session = scoped_session(sessionmaker(autocommit=False,
+                                                     autoflush=False,
+                                                     bind=engine,
+                                                     future=True))
+        return self._session
+
+    def __getattr__(self, name):
+        if name.startswith("__") and name.endswith("__"):
+            raise AttributeError(name)
+        if name == "_is_coroutine_marker":
+            raise AttributeError(name)
+        return getattr(self.session, name)
+
+proxy = SessionProxy()
+session = proxy
 
