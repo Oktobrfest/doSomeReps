a87a5f60f23442b51aa1b6f791815d0786a21531
Oktobrfest <Oktobrfest@users.noreply.github.com>
2026-06-01 23:38:47 -0400

    add audio entrypoint and log errors better

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         ai_integration: "src/main.tsx",
         react_edit_question: "src/edit_question.tsx",
         audio_command_system: "src/audio/audio_command_system.tsx",
+        audio_quiz_entry: "src/audio/audio_quiz_entry.tsx",
       },
     },
   },
